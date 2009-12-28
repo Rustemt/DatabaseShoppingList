b871d4ad6ed0c2a808c63032fd3656f5088f26b4
beepscore <support@beepscore.com>
2009-12-28 14:47:46 -0800

    Implement Add Item view.  Ref Dudney sec 10.2 -> pg 190.

--- Classes/AddItemViewController.h
+++ Classes/AddItemViewController.h
@@ -9,8 +9,22 @@
 #import <UIKit/UIKit.h>
 
 
-@interface AddItemViewController : UIViewController {
+@interface AddItemViewController : UIViewController <UIPickerViewDelegate,
+UIPickerViewDataSource, UITextFieldDelegate> {
 
+    UITextField *itemNameField;
+    UITextField *priceField;
+    UIPickerView *groupPicker;
+    UILabel *statusLabel;
 }
+#pragma mark -
+#pragma mark properties
+
+@property(nonatomic,retain)IBOutlet UITextField *itemNameField;
+@property(nonatomic,retain)IBOutlet UITextField *priceField;
+@property(nonatomic,retain)IBOutlet UIPickerView *groupPicker;
+@property(nonatomic,retain)IBOutlet UILabel *statusLabel;
+
+- (IBAction)addShoppingListItem:(id)sender;
 
 @end
--- Classes/AddItemViewController.m
+++ Classes/AddItemViewController.m
@@ -10,38 +10,98 @@
 
 
 @implementation AddItemViewController
+#pragma mark -
+#pragma mark properties
 
+@synthesize itemNameField;
+@synthesize priceField;
+@synthesize groupPicker;
+@synthesize statusLabel;
+
+#pragma mark -
+#pragma mark initializers / destructors
+- (void)dealloc {
+    [itemNameField release], itemNameField = nil;
+    [priceField release], priceField = nil;
+    [groupPicker release], groupPicker = nil;
+    [statusLabel release], statusLabel = nil;
+    
+    [super dealloc];
+}
+
+
+#pragma mark -
+#pragma mark picker datasource and delegate methods
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return 2;
+}
+
+
+// optional delegate method
+- (NSString *)pickerView:(UIPickerView *)pickerView
+             titleForRow:(NSInteger)row
+            forComponent:(NSInteger)component {
+    if (0 == row) 
+        return @"Groceries";
+    else 
+        return @"Tech";
+}
+
+#pragma mark -
+#pragma mark textField delegate methods.
+// optional method.  Dismiss keyboard when user presses Return
+// Ref Dudney sec 4.6 pg 67
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+// called when textField resigns its first responder status
+// Ref Dudney sec 4.6 pg 67
+-(void)textFieldDidEndEditing:(UITextField *)textField {
+    // ????: do nothing?
+}
+
+#pragma mark -
+- (IBAction)addShoppingListItem:(id)sender {
+    
+}
 
 /*
-// The designated initializer. Override to perform setup that is required before the view is loaded.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
+ // The designated initializer. Override to perform setup that is required before the view is loaded.
+ - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+ if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+ // Custom initialization
+ }
+ return self;
+ }
+ */
 
 /*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView {
-}
-*/
+ // Implement loadView to create a view hierarchy programmatically, without using a nib.
+ - (void)loadView {
+ }
+ */
 
 /*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad {
-    [super viewDidLoad];
-}
-*/
+ // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+ - (void)viewDidLoad {
+ [super viewDidLoad];
+ }
+ */
 
 /*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
+ // Override to allow orientations other than the default portrait orientation.
+ - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+ // Return YES for supported orientations
+ return (interfaceOrientation == UIInterfaceOrientationPortrait);
+ }
+ */
 
 - (void)didReceiveMemoryWarning {
 	// Releases the view if it doesn't have a superview.
@@ -55,9 +115,5 @@
 	// e.g. self.myOutlet = nil;
 }
 
-
-- (void)dealloc {
-    [super dealloc];
-}
 
 @end
