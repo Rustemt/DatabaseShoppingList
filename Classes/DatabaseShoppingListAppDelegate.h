57496c0caa578416d849851858405651693f5042
beepscore <support@beepscore.com>
2010-01-02 22:12:46 -0800

    Copy initializeDb other methods and ivars from sample code.  Ref Dudney sec 10.3

--- Classes/DatabaseShoppingListAppDelegate.h
+++ Classes/DatabaseShoppingListAppDelegate.h
@@ -2,8 +2,12 @@
 //  DatabaseShoppingListAppDelegate.h
 //  DatabaseShoppingList
 //
-//  Created by Steve Baker on 12/28/09.
-//  Copyright Beepscore LLC 2009. All rights reserved.
+//  Created by Chris Adamson on 3/27/09.
+//  Copyright Subsequently and Furthermore, Inc. 2009. All rights reserved.
+//
+//
+//  Licensed with the Apache 2.0 License
+//  http://apache.org/licenses/LICENSE-2.0
 //
 
 #import <UIKit/UIKit.h>
@@ -11,9 +15,11 @@
 @interface DatabaseShoppingListAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
     UIWindow *window;
     UITabBarController *tabBarController;
+    NSString *dbFilePath;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 @property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+@property (nonatomic, retain, readonly) NSString *dbFilePath;
 
 @end
--- Classes/DatabaseShoppingListAppDelegate.m
+++ Classes/DatabaseShoppingListAppDelegate.m
@@ -2,8 +2,12 @@
 //  DatabaseShoppingListAppDelegate.m
 //  DatabaseShoppingList
 //
-//  Created by Steve Baker on 12/28/09.
-//  Copyright Beepscore LLC 2009. All rights reserved.
+//  Created by Chris Adamson on 3/27/09.
+//  Copyright Subsequently and Furthermore, Inc. 2009. All rights reserved.
+//
+//
+//  Licensed with the Apache 2.0 License
+//  http://apache.org/licenses/LICENSE-2.0
 //
 
 #import "DatabaseShoppingListAppDelegate.h"
@@ -13,13 +17,68 @@
 
 @synthesize window;
 @synthesize tabBarController;
+@synthesize dbFilePath;
+
+
+NSString *DATABASE_RESOURCE_NAME = @"shopping";
+NSString *DATABASE_RESOURCE_TYPE = @"db";
+NSString *DATABASE_FILE_NAME = @"shopping.db";
+
+// NSString *dbFilePath;
+
+
+- (BOOL) initializeDb {
+	NSLog (@"initializeDB");
+	// look to see if DB is in known location (~/Documents/$DATABASE_FILE_NAME)
+	//START:code.DatabaseShoppingList.findDocumentsDirectory
+	NSArray *searchPaths =
+    NSSearchPathForDirectoriesInDomains
+    (NSDocumentDirectory, NSUserDomainMask, YES);
+	NSString *documentFolderPath = [searchPaths objectAtIndex: 0];
+	dbFilePath = [documentFolderPath stringByAppendingPathComponent:
+				  DATABASE_FILE_NAME];
+	//END:code.DatabaseShoppingList.findDocumentsDirectory
+	[dbFilePath retain];
+	//START:code.DatabaseShoppingList.copyDatabaseFileToDocuments
+	if (! [[NSFileManager defaultManager] fileExistsAtPath: dbFilePath]) {
+		// didn't find db, need to copy
+		NSString *backupDbPath = [[NSBundle mainBundle]
+                                  pathForResource:DATABASE_RESOURCE_NAME
+                                  ofType:DATABASE_RESOURCE_TYPE];
+		if (backupDbPath == nil) {
+			// couldn't find backup db to copy, bail
+			return NO;
+		} else {
+			BOOL copiedBackupDb = [[NSFileManager defaultManager]
+                                   copyItemAtPath:backupDbPath
+                                   toPath:dbFilePath
+                                   error:nil];
+			if (! copiedBackupDb) {
+				// copying backup db failed, bail
+				return NO;
+			}
+		}
+	}
+	return YES;
+	//END:code.DatabaseShoppingList.copyDatabaseFileToDocuments
+	NSLog (@"bottom of initializeDb");
+}
+
 
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application {
-    
+
+	// copy the database from the bundle if necessary
+	if (! [self initializeDb]) {
+		// TODO: alert the user!
+		NSLog (@"couldn't init db");
+		return;
+	}	
+	
     // Add the tab bar controller's current view as a subview of the window
     [window addSubview:tabBarController.view];
 }
+
 
 
 /*
